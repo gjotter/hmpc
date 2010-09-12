f93ad6035f24ade11459b501f62c0de1c70048af
Gerald Otter <gerald.otter@gmail.com>
2010-09-12 14:21:01 +0200

    Added testing widgets, keymap and exit routine

--- Main.hs
+++ Main.hs
@@ -1,8 +1,11 @@
 {-# OPTIONS -XMultiParamTypeClasses #-} 
 {-# OPTIONS -XTypeSynonymInstances #-} 
 
+import System.Exit
+
 import Control.Monad.State
 import qualified Data.Map as M
+
 import qualified UI.HSCurses.Curses as C
 import qualified UI.HSCurses.CursesHelper as CH
 import qualified UI.HSCurses.Widgets as W
@@ -20,7 +23,13 @@ data HMPCState = HMPCState
     , screens           :: [Screen]
     , global_keys       :: M.Map C.Key (IO ())
     }
-hmpctest = HMPCState "a" ["a","b","c","d"] (M.singleton (C.KeyChar 'q') (return ()))
+hmpctest = HMPCState "a" ["a","b","c","d"] (M.fromList keyList)
+
+keyList = 
+    [(C.KeyChar 'q',    exitHmpc)
+    ,(C.KeyChar 'd',    drawTestWidget testWidget)
+    ,(C.KeyChar 'h',    drawTestWidget testWidget2)
+    ] 
 
 data SimpleLayout = SimpleLayout
 
@@ -32,16 +41,24 @@ instance Layout SimpleLayout Widget where
 
 type Screen = String 
 type Widget = String
+type HMPC = StateT HMPCState IO
 
 wtest :: [Widget]
 wtest = ["a","b","c","d"]
 
-processKey :: C.Key -> StateT HMPCState IO ()
+exitHmpc = CH.end >> exitSuccess
+
+drawTestWidget = W.drawTextWidget (0,0) (10,10) W.DHNormal 
+testWidget = W.TextWidget "test" 0 0 W.defaultTWOptions
+testWidget2 = W.TextWidget "test2" 0 0 W.defaultTWOptions
+
+processKey :: C.Key -> HMPC ()
 processKey k = do si <- get
                   let a = M.lookup k (global_keys si)
+                  let sk = CH.displayKey k
                   liftIO $ maybe (return ()) (liftM id) a
 
-switchScreen :: Int -> StateT HMPCState IO Screen
+switchScreen :: Int -> HMPC Screen
 switchScreen n = do si <- get
                     let ss = screens si
                     let s = ss !! n
@@ -57,8 +74,8 @@ eventloop = do k <- liftIO $ CH.getKey C.refresh
                
                
 main = do CH.start
-          CH.end
           runStateT eventloop hmpctest
+          CH.end
 
 instance Ord C.Key where
     x < y = show x < show y
