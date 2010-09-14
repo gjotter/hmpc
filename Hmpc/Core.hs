62bec236293dddf231d2f1d2df44e133bc0a451d
Gerald Otter <gerald.otter@gmail.com>
2010-09-14 19:50:55 +0200

    working combination of mpd and HMPC monads

--- Hmpc/Core.hs
+++ Hmpc/Core.hs
@@ -2,24 +2,39 @@
              MultiParamTypeClasses, TypeSynonymInstances #-}
              
 module Hmpc.Core 
-    ( hmpc
+    ( 
     ) where
 
 import Network.MPD as MPD
+import Network.MPD.Core
 
 import Control.Monad.State
+import Control.Monad.Error
+
 import qualified Data.Map as M
 
 data HMPCState = HMPCState 
     { current_screen    :: String
     , screens           :: [String]
-    , global_keys       :: M.Map Key (HMPC2 ())
+    , global_keys       :: M.Map Key (HMPC ())
     }
 
 hmpctest = HMPCState "a" ["a","b","c"] (M.fromList keyList)
 
-type HMPC = StateT HMPCState IO 
-type HMPC2 = StateT HMPCState MPD.MPD
+newtype HMPC a = HMPC { 
+                 runHMPC :: StateT HMPCState MPD.MPD a 
+                 } deriving (Functor, Monad, MonadIO, MonadState HMPCState, MonadError MPDError, MonadMPD)
+
+instance MonadMPD (StateT HMPCState MPD) where
+    open = lift open
+    close = lift close 
+    send = lift . send
+    getPassword = lift getPassword
+    receive = lift receive
+    getHandle = lift getHandle
+    setPassword = lift . setPassword
+    getVersion = lift getVersion
+    
 
 data Key = Keyj | Keyk
            deriving (Eq, Ord, Show)
@@ -29,16 +44,21 @@ keyList =
     ,(Keyk,    switchScreen 1)
     ] 
 
-processKey :: Key -> HMPC2 ()
+processKey :: Key -> HMPC ()
 processKey k = do si <- get
                   let a = M.lookup k (global_keys si)
                   maybe (return ()) (id) a
 
-switchScreen :: Int -> HMPC2 ()
+switchScreen :: Int -> HMPC ()
 switchScreen n = do si <- get
                     let ss = screens si
                     let s = ss !! n
+                    o <- play Nothing >> currentSong
+                    liftIO $ maybe (return ()) (putStrLn . show) o
                     put (si { screens = ss, current_screen = s})
 
-printArtist :: HMPC2 ()
-printArtist = liftIO $ putStrLn ",,,"      
+
+liftPlay :: (MonadTrans t, MonadMPD m) => Maybe Int -> t m ()
+liftPlay = lift . play
+
+
